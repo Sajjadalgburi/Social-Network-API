f00df33a0dbd486034353a779434ab7f8a695cc5
Sajjadalgburi <dfgsajjad@gmail.com>
2024-03-29 11:56:13 -0400

    added the route logic

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,3 +9,15 @@ const {
   addFriend,
   removeFriend,
 } = require("../../controllers/usersControllers");
+
+//          /api/users/
+router.route("/").get(getAllUsers).post(createUser);
+
+//          /api/users/:userId
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+//         /api/users/:userId/friends
+router.route(":userId/friends").post(addFriend);
+
+//          /api/users/:userId/friends/:friendId
+router.route(":userId/friends/:friendId").delete(removeFriend);
