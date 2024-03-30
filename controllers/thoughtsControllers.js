d2eb8550442cdc33c1e5c1342ed66dbee07520a9
Sajjadalgburi <dfgsajjad@gmail.com>
2024-03-30 18:22:42 -0400

    Added logic to create a new thought and pushed the created thought's _id to the associated user's thoughts array field

--- controllers/thoughtsControllers.js
+++ controllers/thoughtsControllers.js
@@ -1,4 +1,4 @@
-const { Thought } = require("./../models");
+const { Thought, User } = require("./../models");
 const { reactionSchema } = require("./../models/Reaction");
 
 module.exports = {
@@ -32,6 +32,41 @@ module.exports = {
 
       // Send a JSON response indicating successful thought retrieval
       res.status(200).json({ message: "Thought found", thought });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  },
+  // Assuming you have User and Thought schemas defined
+
+  // In your createThought function
+
+  async createThought(req, res) {
+    try {
+      // Create the thought
+      const newThought = await Thought.create(req.body);
+
+      // If thought not found, send a 404 response and return to exit the function
+      if (!newThought) {
+        return res.status(404).json({ message: `Cannot create new thought` });
+      }
+
+      // Get the user associated with the thought
+      const user = await User.findById(req.body.userId);
+
+      // If user not found, send a 404 response and return to exit the function
+      if (!user) {
+        return res.status(404).json({ message: `User not found` });
+      }
+
+      // Push the created thought's _id to the associated user's thoughts array
+      user.thoughts.push(newThought._id);
+
+      // Save the updated user document
+      await user.save();
+
+      // Send a JSON response indicating successful creation of new thought
+      res.json({ message: `Created new thought: ${newThought._id}!` });
     } catch (err) {
       console.error(err);
       res.status(500).json(err);
