4b6ac5b62c7d0098900bd15e8a85305d1421d1b1
Sajjadalgburi <dfgsajjad@gmail.com>
2024-03-31 18:19:31 -0400

    exporting the module

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -17,7 +17,9 @@ router.route("/").get(getAllUsers).post(createUser);
 router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
 
 //         /api/users/:userId/friends
-router.route(":userId/friends").post(addFriend);
+router.route("/:userId/friends").post(addFriend);
 
 //          /api/users/:userId/friends/:friendId
-router.route(":userId/friends/:friendId").delete(removeFriend);
+router.route("/:userId/friends/:friendId").delete(removeFriend);
+
+module.exports = router;
