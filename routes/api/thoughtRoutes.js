330150c2f215b75fc27e259019945d3ddc147403
Sajjadalgburi <dfgsajjad@gmail.com>
2024-03-31 15:29:06 -0400

    Finising up the api router for thoughts

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -9,3 +9,19 @@ const {
   createReaction,
   removeReaction,
 } = require("../../controllers/thoughtsControllers");
+
+//          /api/thoughts/
+router.route("/").get(getAllThoughts).post(createThought);
+
+//          /api/thoughts/:thoughtId
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+//          /api/thoughts/:thoughtId/reaction
+router.route("/:thoughtId/reactions").post(createReaction);
+
+//          /api/thoughts/:thoughtId/reaction/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
